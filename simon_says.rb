52c67c5cd2eee598a9eb9cef3fc75ddfa5e3a908
Abby Christens <achristens@gmail.com>
2017-11-01 10:52:56 -0400

    start_of_word method working

---
+++ simon_says.rb
@@ -0,0 +1,13 @@
+def start_of_word(string, chars)
+  chars -= 1
+    if chars > 0
+      new_string = []
+      chars = [*0..chars]
+      chars.each do |num|
+         new_string << string[num]
+      end
+      new_string.join("")
+    else
+      string[chars]
+    end
+  end
--- test_simon_says.rb
+++ test_simon_says.rb
@@ -4,22 +4,27 @@ require './simon_says.rb'
 
 class TestSimonSays < MiniTest::Test
   def test_echo_hello
+    skip
     assert_equal "hello", echo("hello")
   end
 
   def test_echo_bye
+    skip
     assert_equal "bye", echo("bye")
   end
 
   def test_shout_hello
+    skip
     assert_equal "HELLO", shout("hello")
   end
 
   def test_shout_multiple_words
+    skip
     assert_equal "HELLO WORLD", shout("hello world")
   end
 
   def test_repeat
+    skip
     assert_equal "hello hello hello", repeat("hello", 3)
   end
 
@@ -30,8 +35,9 @@ class TestSimonSays < MiniTest::Test
   def test_start_of_word_returns_first_two_letters
     assert_equal "Bo", start_of_word("Bob", 2)
   end
-  
+
   def test_first_word
+    skip
     assert_equal "Hello", first_word("Hello World")
   end
 
